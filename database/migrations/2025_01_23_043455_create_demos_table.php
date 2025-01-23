aa5c69fd7aab8752097ca5658c16f78552b72597
Víctor J. <maximovj@outlook.com>
2025-01-22 22:42:10 -0600

    dev-templates: Se crea un BackPack Crud

---
+++ database/migrations/2025_01_23_043455_create_demos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('demos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable()->default('na');
+            $table->string('subtitle')->nullable()->default('na');
+            $table->text('description')->nullable();
+            $table->boolean('is_publish')->nullable()->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('demos');
+    }
+}
