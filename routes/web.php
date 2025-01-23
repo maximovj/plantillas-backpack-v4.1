ce5841f2be9a1188f749f2be448ccb3b054512b5
Víctor J. <maximovj@outlook.com>
2025-01-22 21:57:08 -0600

    * dev-backpack: Se modifica la ruta raíz (/)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect("/".config('backpack.base.route_prefix', 'admin'));
 });
